03e2bdfa2d243699120dcc3e920ac7c779f146ac
Duncan Cook <duncan@yelp.com>
2014-03-13 11:57:55 -0700

    basic map

---
+++ map.js
@@ -0,0 +1,14 @@
+Map = function() {
+	this.height = 10;
+	this.width = 10;
+
+	this.array = [];
+}
+
+
+Map.prototype.canMove = function(x, y) {
+	// returns true or false if we can move into that location
+	return true;
+}
+
+
