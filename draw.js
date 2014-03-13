ae740d07a8debdb7cf37c23b08451d9c568b6129
Duncan Cook <duncan@yelp.com>
2014-03-13 15:12:00 -0700

    works... but rotated

--- draw.js
+++ draw.js
@@ -1,9 +1,21 @@
 var drawGame = function() {
     clearCanvas();
+    drawMap();
     for(var i=0; i<players.length; i++){
         var p = players[i];
         ctx.fillStyle = p.color;
         ctx.fillRect(p.x - 5, p.y - 5, 10, 10);
     }
 
+}
+
+var drawMap = function() {
+    ctx.fillStyle = '#FFF';
+	var game_block_dimensions = map.game_block_dimensions();
+	var map_block_coordinates = map.map_block_coordinates();
+
+	for (var i = 0; i < map_block_coordinates.length; i++) {
+		var point = map_block_coordinates[i];
+		ctx.fillRect(point[0], point[1], game_block_dimensions.width, game_block_dimensions.height);
+	}
 }
--- map.js
+++ map.js
@@ -29,11 +29,35 @@ function Map (game_width, game_height) {
 	this.game_height = game_height;
 }
 
+Map.prototype.game_block_dimensions = function() {
+
+	var game_block_width = this.game_width / this.map_width;
+	var game_block_height = this.game_height / this.map_height;
+
+	// width is x, height is y
+	return {width: game_block_width, height: game_block_height};
+}
+
+Map.prototype.map_block_coordinates = function() {
+	var map_block_coordinates = [];
+	var game_block_dimensions = this.game_block_dimensions();
+	for (var y = 0; y < this.map_height; y++) {
+		for (var x = 0; x < this.map_height; x++) {
+			if (this.canMoveMap(x, y)) {
+				var point = [x * game_block_dimensions.width, y * game_block_dimensions.height]
+				map_block_coordinates.push(point);
+			}
+		}
+	}
+	return map_block_coordinates;
+}
+
 Map.prototype.convert_game_to_map_coordinates = function(game_x, game_y)  {
 	// game_x, game_y are floats
 	// just going to assume that the game_coordinates are always bigger than the map_coordinates
-	var map_x = Math.floor(game_x / (this.game_width / this.map_width))
-	var map_y = Math.floor(game_x / (this.game_height / this.map_height))
+	var game_block_dimensions = this.get_game_block_dimensions();
+	var map_x = Math.floor(game_x / game_block_dimensions[0]);
+	var map_y = Math.floor(game_y / game_block_dimensions[1]);
 
 	return [map_x, map_y];
 }
